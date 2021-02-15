078268914f8177c4ba6dcd1391048b5a19a6ff82
denise-wang <denisew2212@gmail.com>
2021-02-15 13:58:10 -0800

    used banner image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,42 +1,11 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-import PRIME from '../images/PRIME.png'
+import PRIME2 from '../images/PRIME2.svg'
 
 export default function Banner(props) {
     return (
-        <div>
-            <div style={{
-                "padding": "50px",
-                "text-align": "right",
-                "font-family": "Barlow",
-                "font-style": "italic",
-                "font-size": "40px",
-            }}>
-                AN ONLINE EXCLUSIVE BY
-            </div>
-            <div style={{
-                "float": "right", 
-                "margin-right": "50px", 
-                "transform": "translate(-25px, -25px)"
-            }}>
-                <img src={PRIME} alt="PRIME" width="100%"/>
-            </div>
-            <div style={{
-                padding: "25px",
-                "background-color": "black"
-            }}/>
-            <div style={{
-                "text-align": "right",
-                "font-family": "Barlow",
-                "font-style": "italic",
-                "font-size": "40px",
-                "color": "white",
-                "padding": "100px",
-                "padding-right": "50px",
-                "background-color": "black"
-            }}>
-                FROM THE DAILY BRUIN
-            </div>
+        <div style={{"padding": "100px 0px"}}>
+            <img src={PRIME2} alt="PRIME" width="100%"/>
         </div>
     )
  }
